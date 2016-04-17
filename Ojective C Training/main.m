b2a790921eebe76d3c849638faa2d2b07650aeae
Jose Gil Aguilar Reynoso <josegilaguilar@Joses-Mac-mini-2.local>
2016-04-17 09:12:57 -0500

    Initial Commit

---
+++ Ojective C Training/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ojective C Training
+//
+//  Created by Jose Gil Aguilar Reynoso on 4/17/16.
+//  Copyright (c) 2016 josegilaguilarreynosos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
